6ced5a12759257e1b7f6e54e7e5fb6ff9078201e
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-04-04 15:04:06 -0300

    feat: req 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    # informações tiradas de item id 9 de :
+    # /Users/adalbertoramosribeiro/Desktop/Trybe/Projetos/turma-23/sd-023-b-inventory-report/inventory_report/data/inventory.json
+    id = 9
+    nome_do_produto = "eucalyptus globulus"
+    nome_da_empresa = "Target Corporation"
+    data_de_fabricacao = "2020-09-06"
+    data_de_validade = "2024-05-01"
+    numero_de_serie = "GT74 LHWJ FCXL JNQT ZCXM 4761 GWSP"
+    instrucoes_de_armazenamento = "instrucao 9"
+
+    produto = Product(id, nome_do_produto, nome_da_empresa, data_de_fabricacao, data_de_validade, numero_de_serie, instrucoes_de_armazenamento)
+
+    assert produto.id == id
+    assert produto.nome_do_produto == nome_do_produto
+    assert produto.nome_da_empresa == nome_da_empresa
+    assert produto.data_de_fabricacao == data_de_fabricacao
+    assert produto.data_de_validade == data_de_validade
+    assert produto.numero_de_serie == numero_de_serie
+    assert produto.instrucoes_de_armazenamento == instrucoes_de_armazenamento
