03d5ecb6be53bfbcc327d2791077356c328a7870
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-04-05 10:50:53 -0300

    feat: req05

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,22 +1,26 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @staticmethod
     def import_data(file_path: str, report_type: str):
-        # Ler arquivo CSV e armazenar dados em uma lista de dicionários
-        products = []
-        with open(file_path) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                products.append(row)
-
+        # Ler arquivo CSV ou JSON e armazenar dados em uma lista de dicionários
+        if file_path.endswith(".csv"):
+            with open(file_path) as csvfile:
+                reader = csv.DictReader(csvfile)
+                products = [row for row in reader]
+        elif file_path.endswith(".json"):
+            with open(file_path) as jsonfile:
+                products = json.load(jsonfile)
+        else:
+            raise ValueError("Formato de arquivo inválido")
         # Gerar relatório correspondente ao tipo informado
-        if report_type == 'simples':
+        if report_type == "simples":
             return SimpleReport.generate(products)
-        elif report_type == 'completo':
+        elif report_type == "completo":
             return CompleteReport.generate(products)
         else:
-            raise ValueError('Tipo de relatório inválido')
+            raise ValueError("Tipo de relatório inválido")
