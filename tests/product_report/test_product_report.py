856741bfc93df7ff336bd24987947c8cd61084bb
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-04-08 10:50:58 -0300

    fixe comments modify

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -2,7 +2,7 @@ from inventory_report.inventory.product import Product
 
 
 # Este código implementa um teste unitário da classe Product
-#  que verifica se o método str está retornando o resultado esperado
+#  que verifica se o método __repr__ está retornando o resultado esperado
 #  para um objeto de exemplo.
 def test_relatorio_produto():
     product = Product(
