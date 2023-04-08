cd21884fc055dcf4c811eb09da0bbc64a477b867
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-04-08 10:27:34 -0300

    feat: req08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "1",
+        "farinha",
+        50,
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "ao abrigo de luz"
+    )
+    expected_awser = (
+        "O produto farinha fabricado em Farinini por 50 "
+        "com validade até 01-05-2021 precisa ser armazenado "
+        "ao abrigo de luz."
+    )
+    assert str(product) == expected_awser
